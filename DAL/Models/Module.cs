21884a5fd3e5302224981879508c4076fcc0da6d
Faraz Ahmed Shaikh <lets_play879@yahoo.com>
2022-07-26 09:46:50 -0700

    fixes

---
+++ DAL/Models/Module.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Module
+    {
+        public int ModuleID { get; set; }
+        public int? ApplicationID { get; set; }
+        public string? ModuleName { get; set; }
+        public string? ModuleDescription { get; set; }
+        public string? ModuleIcon { get; set; }
+        public int? ModuleSortOrder { get; set; }
+        public int? CreatedBy { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
