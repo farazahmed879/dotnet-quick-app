3c556ad8d155e7e4611c553fe922b9758f93d526
Saleem-Developer <saleemhaq12@gmail.com>
2022-07-29 17:02:33 +0500

    save group endpoint, isgroupexist endpoint, group authorization endpoints are done

---
+++ QuickApp/ViewModels/SavePagesVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickApp.ViewModels
+{
+    public class SavePagesVM
+    {
+        public string RoleID { get; set; }
+        public string ModuleID { get; set; }
+        public string PageID { get; set; }
+        public bool canView { get; set; }
+        public bool canInsert { get; set; }
+        public bool canUpdate { get; set; }
+        public bool canAuthorize { get; set; }
+        public bool canReject { get; set; }
+        public bool canDelete { get; set; }
+    }
+}
